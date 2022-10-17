080e57dd8dec7c1d238fad171c549efca33b9dbf
Bhargav Sakaria <bhargav.sakaria@creolestudios.com>
2022-10-17 18:21:16 +0530

    [Feature]-practice-anguler-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlert } from './success/success.component';
+import { WarningAlert } from './warning/warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlert,
+    WarningAlert
   ],
   imports: [
     BrowserModule
